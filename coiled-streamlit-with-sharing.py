4954f07312924689762deb5029865df512a19391
Richard Pelgrim <68642378+rrpelgrim@users.noreply.github.com>
2021-07-26 09:45:54 +0200

    Create coiled-streamlit-with-sharing.py

---
+++ coiled-streamlit-with-sharing.py
@@ -0,0 +1,9 @@
+import coiled
+import dask
+import dask.dataframe as dd
+import folium
+import streamlit as st
+from dask.distributed import Client
+from folium.plugins import HeatMap
+from streamlit_folium import folium_static
+
